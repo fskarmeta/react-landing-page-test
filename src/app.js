04f589e78fa46f29466c3e364ef87098e4ec57ec
Guaaan <72053761+Guaaan@users.noreply.github.com>
2020-10-29 15:14:49 -0300

    navbar listo

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 import React from "react"
-//import  Navbar from "./navbar";
+import  Navbar from "./navbar";
 import  Jumbotron  from "./jumbotron";
 //import  Card  from "./cards";
 //import  Footer from "./footer";
 
+
+
 const App = () => (
     <>
-      {/* <Navbar logo="Start Bootstrap" /> */}
+      <Navbar logo="Start Bootstrap" features="about" home="home" pricing="services" contact="contact"/>
       <div className="container">
         <div className="row">
+
           <Jumbotron
             title="A Warm Welcome!"
-            description=""
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquet nisl facilisis, ultrices lorem eget, gravida nibh. Proin convallis mauris sit amet porta rhoncus. Cras ultricies diam libero. Sed pharetra vel tellus eu feugiat. Nunc sollicitudin, odio at semper mollis, erat urna laoreet ex, eu pulvinar libero elit sed mauris. Morbi tempus mauris"
             button="Learn More"
           />
         </div>
--- src/navbar.js
+++ src/navbar.js
@@ -1,21 +1,31 @@
-import React from "react";+
+import React from "react";
 
-const Navbar = (props) =>{
+const Navbar = (props) => {
     return(
-                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="#">Navbar</a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div className="navbar-nav">
-              <a className="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-              <a className="nav-item nav-link" href="#">Features</a>
-              <a className="nav-item nav-link" href="#">Pricing</a>
-              <a className="nav-item nav-link disabled" href="#">Disabled</a>
-            </div>
-          </div>
-        </nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+      <a className="navbar-brand" href="#">{props.logo}</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">{props.features} <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">{props.home}</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">{props.pricing}</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link " href="#">{props.contact}</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+    </nav>
     );
 };
 export default Navbar
